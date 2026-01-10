b17f8372cb764bdf54b1f96fde5dbcee76b18228
Petrus Maas <pjmaasdev@gmail.com>
2026-01-10 13:41:34 +0200

    Write test for Firestore integration

---
+++ test/firestore_integration_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  test('Creating a post should add a document to the posts collection', () async {
+    final db = FakeFirebaseFirestore();
+
+    final post = <String, dynamic>{
+      "title": "My Test Post",
+      "body":
+          "If you want to make sure your test is working you've come to the right place!",
+      "createdAt": FieldValue.serverTimestamp(),
+    };
+
+    final DocumentReference docRef = await db.collection("posts").add(post);
+
+    final DocumentSnapshot snapshot = await db
+        .collection("posts")
+        .doc(docRef.id)
+        .get();
+
+    expect(snapshot.exists, true);
+    expect(snapshot.get('title'), "My Test Post");
+    expect(snapshot.get('body'), contains("right place!"));
+
+    print('Test Passed: Document added with ID: ${docRef.id}');
+  });
+}
